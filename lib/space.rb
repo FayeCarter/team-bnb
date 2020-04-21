98eefd7a77dee3c5ed7531469071b6d1df7b4ff6
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-21 14:26:21 +0100

    G: Creates and inserts new space into db

--- lib/space.rb
+++ lib/space.rb
@@ -1,3 +1,16 @@
+require 'PG'
+
 class Space
-  
+  attr_reader :id, :name
+
+  def initialize(id:, name:)
+    @id = id
+    @name = name
+  end
+
+  def self.create(name:)
+    conn = PG.connect(dbname: 'bnb_test')
+    result = conn.exec("INSERT INTO spaces (name) VALUES('#{name}') RETURNING id, name;")
+    return Space.new(id: result[0]['id'], name: result[0]['name'])
+  end
 end
