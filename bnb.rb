41bb533e2a70a8dcc82c7ca70caca481fa1a7e78
Harry Turnbull <hturnbull@live.co.uk>
2020-04-21 12:15:35 +0100

    G: Listing cottages, can see "Cottage has been listed"

--- bnb.rb
+++ bnb.rb
@@ -5,4 +5,13 @@ class BnB < Sinatra::Base
   get '/' do
     "Hello World!"
   end
+
+  get '/spaces/new' do
+    erb :'spaces/new'
+  end
+
+  post '/spaces/new' do
+    "Cottage has been listed"
+  end
+
 end
