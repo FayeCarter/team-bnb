72a59b4d9f8d795bf9e977b4083fe431b086c70f
Harry Turnbull <hturnbull@live.co.uk>
2020-04-21 10:53:14 +0100

    spec_helper set up

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+
+require 'simplecov'
+require 'simplecov-console'
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+require File.join(File.dirname(__FILE__), '..', 'bnb.rb')
+
+Capybara.app = BnB
+
+RSpec.configure do |config|
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::Console,
+    # Want a nice code coverage website? Uncomment this next line!
+    # SimpleCov::Formatter::HTMLFormatter
+    ])
+  SimpleCov.start
+end
