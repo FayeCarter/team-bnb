120937ae894ca4606caf2f5a8b6ea206c202b70d
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-21 12:21:03 +0100

    R: listing lighthouse

--- spec/features/add_space_spec.rb
+++ spec/features/add_space_spec.rb
@@ -5,4 +5,11 @@ feature 'add a space' do
     click_on 'Submit'
     expect(page).to have_content 'Cottage has been listed'
   end
+
+  scenario 'visit the new space page, enter name: "Lighthouse", then see Lighthouse has been listed' do
+    visit '/spaces/new'
+    fill_in('name', with: 'Lighthouse')
+    click_on 'Submit'
+    expect(page).to have_content 'Lighthouse has been listed'
+  end
 end
