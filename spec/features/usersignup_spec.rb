aebac6bb83b3a744d7cfea69fad4ec47b3b80c1f
Graham <grahamalconer@icloud.com>
2020-04-22 12:27:04 +0100

    R: Signup, first test

---
+++ spec/features/usersignup_spec.rb
@@ -0,0 +1,11 @@
+feature 'Signup' do
+  scenario 'Graham signs up and sees his name' do
+    visit '/users/new'
+    fill_in('name', with: 'Graham')
+    fill_in('password', with: 'graham_example')
+    fill_in('email', with: 'gman@gmail.com')
+    click_on 'Signup'
+    expect(current_path).to be('/spaces')
+    expect(page).to have_content'Graham'
+  end
+end
