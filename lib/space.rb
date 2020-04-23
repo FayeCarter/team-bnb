aa8c53374e224a4483c9b3294e54427f29669e87
Edward Phillips <edd0ddi0@gmail.com>
2020-04-23 11:14:10 +0100

    RF: linting

--- lib/space.rb
+++ lib/space.rb
@@ -12,12 +12,22 @@ class Space
   def self.create(name:, price:)
     connection = PG.connect(dbname: 'bnb_test')
     result = connection.exec("INSERT INTO spaces (name, price) VALUES('#{name}', '#{price}') RETURNING  id, name, price;")
-    return Space.new(id: result[0]['id'], name: result[0]['name'], price: result[0]['price'])
+    Space.new(
+      id: result[0]['id'],
+      name: result[0]['name'],
+      price: result[0]['price']
+    )
   end
 
   def self.all
     connection = PG.connect(dbname: 'bnb_test')
     result = connection.exec('SELECT * FROM spaces;')
-    result.map {|space| Space.new(id: space['id'], name: space['name'], price: space['price'])} 
+    result.map do |space|
+      Space.new(
+        id: space['id'],
+        name: space['name'],
+        price: space['price']
+      )
+    end
   end
 end
