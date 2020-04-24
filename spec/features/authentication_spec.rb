b5d9b4dfec77091c3e1d300f8fd25c05f3c5e75f
Harry Turnbull <hturnbull@live.co.uk>
2020-04-24 11:58:05 +0100

    G: sanity check test for wrong email works also

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -18,4 +18,14 @@ feature 'authentication' do
     expect(page).to have_content('Incorrect email or password')
     expect(page).to_not have_content('Sinatra')
   end
+
+  scenario 'existing user signs in with wrong email and gets error message' do
+    existing_user = User.create(first_name: 'john', last_name: 'smith', email: 'johnsmith@smith.com', password: 'johniscool')
+    visit('sessions/new')
+    fill_in('password', with: 'johniscool')
+    fill_in('email', with: 'wrongemail@smith.com')
+    click_on 'login'
+    expect(page).to have_content('Incorrect email or password')
+    expect(page).to_not have_content('Sinatra')
+  end
 end
