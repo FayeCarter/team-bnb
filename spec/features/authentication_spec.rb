3d60e7f61c722cefa811f5df5d7b5b8eed86c790
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-04-24 12:11:07 +0100

    R user signs out and gets sign  out message

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -28,4 +28,16 @@ feature 'authentication' do
     expect(page).to have_content('Incorrect email or password')
     expect(page).to_not have_content('Sinatra')
   end
+
+  scenario 'user signs out and gets sign  out message' do 
+    existing_user = User.create(first_name: 'john', last_name: 'smith', email: 'johnsmith@smith.com', password: 'johniscool')
+    visit('sessions/new')
+    fill_in('password', with: 'johniscool')
+    fill_in('email', with: 'johnsmith@smith.com')
+    click_on 'login'
+    expect(current_path).to eq '/'
+    click_on 'logout'
+    expect(page).to have_content("You have been signed out")
+    expect(page).to_not have_content("john")
+  end
 end
