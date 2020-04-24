6f67dd88259203ee06bf32f984fc7e4982d8e66a
Harry Turnbull <hturnbull@live.co.uk>
2020-04-24 11:48:13 +0100

    R: existing user signs in with wrong password and gets error message

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -9,5 +9,14 @@ feature 'authentication' do
     expect(page).to_not have_content('Sinatra')
   end
 
+  scenario 'existing user signs in with wrong password and gets error message' do
+    existing_user = User.create(first_name: 'john', last_name: 'smith', email: 'johnsmith@smith.com', password: 'johniscool')
+    visit('sessions/new')
+    fill_in('password', with: 'wrongpassword')
+    fill_in('email', with: 'johnsmith@smith.com')
+    click_on 'login'
+    expect(page).to have_content('Incorrect email or password')
+    expect(page).to_not have_content('Sinatra')
 
+  end
 end
